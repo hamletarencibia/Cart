40fcd0204c5cc9b56fda24f1cfbd2d530e488c91
hamletarencibia <hamletarencibia@gmail.com>
2022-08-09 12:13:59 -0400

    feat(cart): add id and image to mocked products

--- src/mock/products.ts
+++ src/mock/products.ts
@@ -1,24 +1,31 @@
-import { ProductInCart } from "../contracts/ProductInCart";
+import { IProductInCart } from "../contracts/ProductInCart";
 
-export const mocked_products: ProductInCart[] = [
+export const mocked_products: IProductInCart[] = [
     {
         product: {
+            id: 'pack-1',
             name: 'My Christmas pack',
             price: 71.20,
             products: [
                 {
+                    id: 'product-3',
                     name: 'Cardboard box',
                     price: 0,
+                    image: '/img/box.png',
                     attributes: ['container']
                 },
                 {
+                    id: 'product-4',
                     name: 'Unisex Short Sleeve T-Shirt',
                     price: 0,
+                    image: '/img/tshirt.png',
                     attributes: ['Green', 'Small']
                 },
                 {
+                    id: 'product-5',
                     name: 'Basic bottle',
                     price: 0,
+                    image: '/img/bottle.png',
                     attributes: ['Blue']
                 }
             ]
@@ -27,15 +34,19 @@ export const mocked_products: ProductInCart[] = [
     },
     {
         product: {
+            id: 'product-1',
             name: 'Basic T-Shirt',
+            image: '/img/tshirt.png',
             price: 13.50
         },
         quantity: 10
     },
     {
         product: {
+            id: 'product-2',
             name: 'Woman bottle',
-            price: 36.50
+            price: 36.50,
+            image: '/img/bottle.png'
         },
         quantity: 20
     }
